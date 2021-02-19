e85c650e7eeb86f83a8ef78b25e16bf8c7506096
haayun <kng06325@gmail.com>
2021-02-19 23:20:53 +0900

    Customer List Refresh Module

--- kim(ReactProjectTutorial)/management/client/src/App.js
+++ kim(ReactProjectTutorial)/management/client/src/App.js
@@ -27,9 +27,22 @@ const styles = theme => ({
 
 class App extends Component {
 
-  state = { // 변경될 수 있는 변수
-    customers: "",
-    completed: 0
+  constructor(props) {
+    super(props);
+    this.state = {
+      customer: '',
+      completed: 0
+    }
+  }
+
+  stateRefresh = () => {
+    this.setState({
+      customers: '',
+      completed: 0
+    });
+    this.callApi()
+      .then(res => this.setState({customers: res}))
+      .catch(err => console.log(err));
   }
 
   componentDidMount() {
@@ -78,7 +91,7 @@ class App extends Component {
             </TableBody>
           </Table>
         </Paper>
-        <CustomerAdd/>
+        <CustomerAdd stateRefresh={this.stateRefresh}/>
       </div>
     );
   }
